b3d03f24d4534e6607615b347465b83983e01965
Matthew Nicholls <matthew.nicholls95@hotmail.co.uk>
2017-05-19 14:16:24 +0100

    planetgen: Use a build script for linking SDL2 on Windows
      * lib/target/ - contains library files
    
      * bin/target/ - contains DLL files
    
    Where "target" is the build target as understood by Cargo, e.g.:
    
        x86_64-pc-windows-msvc
    
    Based on the build script given in the rust-sdl2 readme:
    
        https://github.com/AngryLawyer/rust-sdl2

---
+++ build.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    if target.contains("pc-windows") {
+        let profile = env::var("PROFILE").unwrap();
+        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        let mut lib_dir = manifest_dir.clone();
+        let mut dll_dir = manifest_dir.clone();
+        let mut copy_dir = manifest_dir.clone();
+        lib_dir.push("lib");
+        lib_dir.push(target.clone());
+        dll_dir.push("bin");
+        dll_dir.push(target.clone());
+        copy_dir.push("target");
+        copy_dir.push(profile);
+
+        println!("cargo:rustc-link-search=all={}", lib_dir.display());
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = copy_dir.clone();
+
+            if let Some(file_name) = file_name_result {
+                let file_name = file_name.to_str().unwrap();
+                if file_name.ends_with(".dll") {
+                    new_file_path.push(file_name);
+                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
+                }
+            }
+        }
+    }
+}
